47beb0e1b040f0357b8b368da4cbbe403a8d7ad2
odkok <wilsonhung2002@gmail.com>
2022-03-11 00:48:27 +0800

    complete basic function of encoder_node

--- encoder_driver/src/encoder_node.cpp
+++ encoder_driver/src/encoder_node.cpp
@@ -1,5 +1,5 @@
 #include "ros/ros.h"
-#include "std_msgs/Int32.h"
+#include "std_msgs/UInt32.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,68 +7,98 @@
 #include <unistd.h>
 #include <stdint.h>
 
-int main(int argc, char **argv){
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+int main(int argc, char **argv)
+{
     ros::init(argc, argv, "encoder_node");
+
+    ROS_INFO("Init node");
 
     ros::NodeHandle nh;
 
-    int s, i; 
-	int nbytes;
-	struct sockaddr_can addr;
-	struct ifreq ifr;
-	struct can_frame frame;
+    int s, i;
+    int nbytes;
+    struct sockaddr_can addr;
+    struct ifreq ifr;
+    struct can_frame frame;
 
-    if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
-		perror("Socket");
-		return 1;
-	}
+    if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0)
+    {
+        perror("Socket");
+        return 1;
+    }
 
-    strcpy(ifr.ifr_name, "vcan0" );
-	ioctl(s, SIOCGIFINDEX, &ifr);
+    strcpy(ifr.ifr_name, "vcan0");
+    ioctl(s, SIOCGIFINDEX, &ifr);
 
-	memset(&addr, 0, sizeof(addr));
-	addr.can_family = AF_CAN;
-	addr.can_ifindex = ifr.ifr_ifindex;
+    memset(&addr, 0, sizeof(addr));
+    addr.can_family = AF_CAN;
+    addr.can_ifindex = ifr.ifr_ifindex;
 
-	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-		perror("Bind");
-		return 1;
-	}
+    if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("Bind");
+        return 1;
+    }
 
-    ros::Publisher pub = nh.advertise<std_msgs::Int32>("encoder_node/count", 1);
+    ROS_INFO("Socket Setup Successfully");
 
-    while(ros::ok()){
+    ros::Publisher pub = nh.advertise<std_msgs::UInt32>("encoder_node/count", 1);
 
+    while (ros::ok())
+    {
+
+        ROS_INFO("Waiting a frame...");
         nbytes = read(s, &frame, sizeof(struct can_frame));
 
-        if(nbytes > 0){
-
+        if (nbytes > 0)
+        {
             uint8_t length = frame.data[0];
-
-            if(length != frame.can_dlc){
-                continue;
-            }
-
             uint8_t deviceId = frame.data[1];
             uint8_t cmdId = frame.data[2];
 
-            uint32_t reading;
-            for(int i=3; i<length -3;++i){
+            if (length != frame.can_dlc || length <= 3)
+            {
+                ROS_INFO("Invalid Packet, length=%d, can_dlc=%u", length, frame.can_dlc)
+                continue;
+            }
+
+            for (int i = 0; i < length; ++i)
+            {
+                ROS_INFO("Pos %d: %x", i, frame.data[i]);
+            }
+
+            uint32_t reading = 0;
+            for (int i = 3; i < length; ++i)
+            {
                 reading |= frame.data[i] << 8 * (i - 3);
             }
-            
 
-            std_msgs::Int32 msg;
+            std_msgs::UInt32 msg;
 
             msg.data = reading;
 
-            ROS_INFO("%d published", count);
+            ROS_INFO("%x published", reading);
 
             pub.publish(msg);
-        }
 
-        
+            ros::spinOnce();
+        }
     }
+
+    if (close(s) < 0)
+    {
+        perror("Close");
+        return 1;
+    }
+
+    ros::shutdown();
 
     return 0;
 }
